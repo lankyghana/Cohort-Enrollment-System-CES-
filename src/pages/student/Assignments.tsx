fbfdcde6178a9d536420040e2993b5cc7c21d83f
lankyghana <dktakyi001@st.ug.edu.gh>
2025-11-20 03:34:05 +0000

    Updated Assignments.tsx: tweaked component logic, changed imports.

--- src/pages/student/Assignments.tsx
+++ src/pages/student/Assignments.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import AssignmentsService from '@/services/assignments'
 import { Link } from 'react-router-dom'
 import { Card } from '@/components/ui/Card'
